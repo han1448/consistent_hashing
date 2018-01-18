f5dd7713127e97f9fc8778bf864b03bb8064930e
seung hyun han <han1448@hanmail.net>
2018-01-18 22:35:50 +0900

    add java doc

--- src/main/java/com/oppalove/consistent/ConsistentHash.java
+++ src/main/java/com/oppalove/consistent/ConsistentHash.java
@@ -8,31 +8,51 @@ import java.util.Random;
 import java.util.TreeMap;
 
 @Slf4j
-public class ConsistentHash {
+public class ConsistentHash<T> {
     private static final int MAX_NODE = 10000;
     private static final int DEFAULT_REPLICA = 1;
     private int virtualNodeCount;
     private int maxNodeCount;
     private int currentNodeCount;
-    private NavigableMap<Integer, Node> hashRing = new TreeMap<>();
+    private NavigableMap<Integer, T> hashRing = new TreeMap<>();
     private Random random = new Random();
 
+    /**
+     * Default constructor.
+     */
     public ConsistentHash() {
         this.virtualNodeCount = DEFAULT_REPLICA;
         this.maxNodeCount = MAX_NODE;
     }
 
+    /**
+     * Constructor that can be set virtualNodeCount.
+     * If you add new node, it will be created randomly any index as the number of virtualNodeCount.
+     *
+     * @param virtualNodeCount the number of virtual count per each node.
+     */
     public ConsistentHash(int virtualNodeCount) {
         this.virtualNodeCount = virtualNodeCount;
         this.maxNodeCount = MAX_NODE;
     }
 
+    /**
+     * Constructor that can be set virtualNodeCount and maxNode.
+     *
+     * @param virtualNodeCount the number of virtual node per each node.
+     * @param maxNode          the number of maximum node including virtual node.
+     */
     public ConsistentHash(int virtualNodeCount, int maxNode) {
         this.virtualNodeCount = virtualNodeCount;
         this.maxNodeCount = maxNode;
     }
 
-    public void add(Node node) {
+    /**
+     * Add new node.
+     *
+     * @param node T node object.
+     */
+    public void add(T node) {
         currentNodeCount++;
 
         if ((currentNodeCount * virtualNodeCount) >= maxNodeCount * 0.9) {
@@ -44,28 +64,39 @@ public class ConsistentHash {
             while (hashRing.containsKey(nodeIdx)) {
                 nodeIdx = random.nextInt(maxNodeCount);
             }
-            log.debug("adding a node({}) -> {}", node.ip, nodeIdx);
+            log.debug("adding a node({}) -> {}", node.toString(), nodeIdx);
             hashRing.put(nodeIdx, node);
         }
     }
 
-    public void remove(Node node) {
+    /**
+     * Remove node from hash.
+     *
+     * @param node T node object.
+     */
+    public void remove(T node) {
 
         final Integer[] keyNode = new Integer[virtualNodeCount];
-        int idx=0;
-        for (Map.Entry<Integer, Node> entry : hashRing.entrySet()) {
+        int idx = 0;
+        for (Map.Entry<Integer, T> entry : hashRing.entrySet()) {
             if (entry.getValue().equals(node)) {
-                log.debug("removing a node({}) -> {}", node.ip, entry.getKey());
+                log.debug("removing a node({}) -> {}", node.toString(), entry.getKey());
                 keyNode[idx++] = entry.getKey();
             }
         }
 
-        for (int i=0; i < keyNode.length; i++) {
+        for (int i = 0; i < keyNode.length; i++) {
             hashRing.remove(keyNode[i]);
         }
     }
 
-    public Node getNode(String key) {
+    /**
+     * Get node from the given key.
+     *
+     * @param key key data.
+     * @return T node.
+     */
+    public T getNode(String key) {
         int hashKey = key.hashCode() % maxNodeCount;
         log.debug("getNode() -> {}", hashKey);
         if (hashRing.ceilingEntry(hashKey) == null)
--- src/main/java/com/oppalove/consistent/Main.java
+++ src/main/java/com/oppalove/consistent/Main.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        ConsistentHash consistentHash = new ConsistentHash(100);
+        ConsistentHash<Node> consistentHash = new ConsistentHash<>(100);
         consistentHash.add(new Node("127.0.0.1", "8080"));
         consistentHash.add(new Node("127.0.0.2", "8080"));
         consistentHash.add(new Node("127.0.0.3", "8080"));
@@ -22,13 +22,13 @@ public class Main {
         Node node4 = new Node("127.0.0.4", "8080");
         consistentHash.add(node4);
 
-         node = consistentHash.getNode("Hi");
+        node = consistentHash.getNode("Hi");
         log.debug("Node is {}", node.ip);
 
-         node1 = consistentHash.getNode("Hello");
+        node1 = consistentHash.getNode("Hello");
         log.debug("Node is {}", node1.ip);
 
-         node2 = consistentHash.getNode("GoodBye");
+        node2 = consistentHash.getNode("GoodBye");
         log.debug("Node is {}", node2.ip);
 
         consistentHash.remove(node4);
